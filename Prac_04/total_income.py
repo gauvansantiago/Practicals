e57c7d96f01431c451b8aaf59532cccb99054f78
gsantia1 <gauvan.santiago@my.jcu.edu.au>
2020-08-27 15:53:49 +1000

    refactor total_income.py and display neatly

---
+++ Prac_04/total_income.py
@@ -0,0 +1,28 @@
+"""
+Gauvan Santiago Prac_04 Task 02
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    numb_of_months = int(input("How many months? "))
+
+    for month in range(1, numb_of_months + 1):
+        income = float(input("Enter income for month " + str(month) + ": "))
+        incomes.append(income)
+
+    print_report(incomes)
+
+
+def print_report(incomes):
+
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month, income in enumerate(incomes):
+        total += income
+        print("Month {:2} - Income: ${:10.2f} \
+        Total: ${:10.2f}".format(month + 1, income, total))
+
+
+main()
