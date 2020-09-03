0a776ebdf93e30cd3c600b8ce43613ec8ab77c9a
gsantia1 <gauvan.santiago@my.jcu.edu.au>
2020-09-03 15:17:54 +1000

    Started state_names

---
+++ Prac_05/state_names.py
@@ -0,0 +1,19 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+State names in a dictionary
+File has been reformatted and state inputs can be any case
+"""
+
+states = {"QLD": "Queensland", "NSW": "New South Wales",
+               "NT": "Northern Territory", "WA": "Western Australia",
+               "ACT": "Australian Capital Territory", "VIC": "Victoria",
+               "TAS": "Tasmania"}
+print(states)
+
+state = input("Enter short state: ").upper()
+while state != "":
+    if state in states:
+        print(state, "is", states[state])
+    else:
+        print("Invalid short state")
+    state = input("Enter short state: ").upper()
